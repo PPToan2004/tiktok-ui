98d7f10b0662a6fcc65d2d7b64dc93c8b5b1bd51
Phuoc Toan <mr.toan2004@gmail.com>
2026-01-20 22:06:42 +0700

    Building a Sidebar UI #1

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types'
 import classNames from 'classnames/bind';
-import styles from './DefaultLayout.module.scss';
+import Sidebar from '~/layouts/components/Sidebar';
 import Header from '~/layouts/components/Header';
-import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
 
 const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
